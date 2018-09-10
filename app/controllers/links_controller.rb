8bc11963df944a0e5eaa373cd22d8d9232f70a4b
Nick Brown <nickrossbrown@gmail.com>
2018-09-09 17:45:56 -0700

    added before_action to Linkcontroller for signing out

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,31 +1,31 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_link, only: [:show, :edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
-  def index
+    def index
     @links = Link.all
-  end
+    end
 
-  # GET /links/1
-  # GET /links/1.json
-  def show
-  end
+    # GET /links/1
+    # GET /links/1.json
+    def show
+    end
 
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
+    # GET /links/new
+    def new
+    @link = current_user.links.build
+    end
 
-  # GET /links/1/edit
-  def edit
-  end
+    # GET /links/1/edit
+    def edit
+    end
 
-  # POST /links
-  # POST /links.json
-  def create
-    @link = Link.new(link_params)
-
+    # POST /links
+    # POST /links.json
+    def create
+    @link = current_user.links.build(link_params)
     respond_to do |format|
       if @link.save
         format.html { redirect_to @link, notice: 'Link was successfully created.' }
@@ -35,11 +35,11 @@ class LinksController < ApplicationController
         format.json { render json: @link.errors, status: :unprocessable_entity }
       end
     end
-  end
+    end
 
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
-  def update
+    # PATCH/PUT /links/1
+    # PATCH/PUT /links/1.json
+    def update
     respond_to do |format|
       if @link.update(link_params)
         format.html { redirect_to @link, notice: 'Link was successfully updated.' }
@@ -49,19 +49,19 @@ class LinksController < ApplicationController
         format.json { render json: @link.errors, status: :unprocessable_entity }
       end
     end
-  end
+    end
 
-  # DELETE /links/1
-  # DELETE /links/1.json
-  def destroy
+    # DELETE /links/1
+    # DELETE /links/1.json
+    def destroy
     @link.destroy
     respond_to do |format|
       format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
       format.json { head :no_content }
     end
-  end
+    end
 
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
